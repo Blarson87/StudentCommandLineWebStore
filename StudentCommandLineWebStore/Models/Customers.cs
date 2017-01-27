2bd70788a82b01de9dc4730c68c10b48712da7c9
Zackary Lowery <zlowery@xcjs.com>
2017-01-27 02:56:14 +0000

    Renamed the root namespace from ConsoleApplication4 to StudentCommandLineWebStore to match the repository name.

--- StudentCommandLineWebStore/Models/Customers.cs
+++ StudentCommandLineWebStore/Models/Customers.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConsoleApplication4.Models.Types;
+using StudentCommandLineWebStore.Models.Types;
 
-namespace ConsoleApplication4.Models
+namespace StudentCommandLineWebStore.Models
 {
     class Customers
     {
--- StudentCommandLineWebStore/Models/Types/Customer.cs
+++ StudentCommandLineWebStore/Models/Types/Customer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication4.Models.Types
+namespace StudentCommandLineWebStore.Models.Types
 {
     class Customer
     {
