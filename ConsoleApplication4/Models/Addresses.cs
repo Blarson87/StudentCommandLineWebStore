ef4967ba0d1a8dc5efbb9689e9abe6c22da6f82d
Brian Larson <Brian Larson>
2017-01-26 21:25:54 -0500

    Imported base project from file system

---
+++ ConsoleApplication4/Models/Addresses.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication4.Models.Types;
+
+namespace ConsoleApplication4.Models
+{
+    class Addresses
+    {
+        public static List<Address> All = new List<Address>
+        {
+          new Address()
+          {
+              ID = 1,
+              StreetName = "97628 Westend Court",
+              City = "Pueblo",
+              State = "Colorado",
+              ZipCode = "81005", 
+              Country = "United States"
+          },
+          new Address()
+          {
+              ID = 2,
+              StreetName = "0212 Aberg Junction",
+              City = "Richmond",
+              State = "Virginia",
+              ZipCode = "23285",
+              Country = "United States"
+          },
+          new Address()
+          {
+              ID = 3,
+              StreetName = "5 Dapub Street",
+              City = "Columbia",
+              State = "South Carolina",
+              ZipCode = "29220",
+              Country = "United States"
+          },
+          new Address()
+          {
+              ID = 4,
+              StreetName = "92 Nova Port",
+              City = "Sacromento",
+              State = "California",
+              ZipCode = "958865",
+              Country = "United States"
+          },
+            new Address()
+          {
+              ID = 5,
+              StreetName = "93665 Sugar Place",
+              City = "Seattle",
+              State = "Washington",
+              ZipCode = "98133",
+              Country = "United States"
+          }
+        };
+
+
+    }
+}
---
+++ ConsoleApplication4/Models/Types/Order.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4.Models.Types
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? ShipDate
+        {
+            get
+            {
+                return _shipDate;
+            }
+            set
+            {
+                if (!OrderDate.HasValue || !value.HasValue)
+                {
+                    throw new NullReferenceException("Order date and ship date required!");
+                }
+                
+                if (OrderDate > value)
+                {
+                    throw new FormatException("Order date cannot be earlier than ship date!");
+                }
+
+                _shipDate = value;
+            }
+        }
+
+        public enum Statuses
+        {
+            Cart,
+            Ordered,
+            Shipped
+        }
+
+        public Dictionary<Product, int> Products
+        {
+            get
+            {
+                return _products;
+            }
+        }
+
+        private DateTime? _shipDate;
+        private Dictionary<Product, int> _products;
+
+        public Order() { }
+
+        public void AddProduct(Product product)
+        {
+            if(product.Quantity == 0)
+            {
+                return;
+            }
+                
+            if (_products.ContainsKey(product))
+            {
+                _products[product]++;
+            }
+            else
+            {
+                _products.Add(product, 1);
+            }
+
+            product.Quantity--;
+        }
+        
+        public Statuses GetStatus()
+        {
+            Statuses current;
+
+            if(!OrderDate.HasValue && !ShipDate.HasValue)
+            {
+                current = Statuses.Cart;
+            }
+            else if( OrderDate.HasValue && !ShipDate.HasValue)
+            {
+                current = Statuses.Ordered;
+            }
+            else
+            {
+                current = Statuses.Shipped;
+            }
+
+            return current;
+        }
+
+
+    }
+}
